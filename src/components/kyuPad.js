682c636837b94a7a5bdeef6673addd52e182db47
Kathy Dieser <katdiese@gmail.com>
2016-04-25 12:29:47 -0600

    putting in react Components

--- src/components/kyuPad.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class randomForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {input: ''}
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Component } from 'react';
+import ReactDom from 'react-dom';
+import RandomForm from './components/kyuPad';
+import NavButtons from './components/navButtons';
+import SpheroConnectButton from './components/spheroConnectButton';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <NavButtons />
+      <RandomForm />
+      <SpheroConnectButton />
+      </div>
+    );
+  }
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-
-    <App />
-
-  , document.querySelector('.container'));
+ReactDom.render(
+  <App />,
+  document.querySelector('.container')
+)
